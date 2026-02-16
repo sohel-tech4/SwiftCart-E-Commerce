91f8f5d197ded83eee2b958447721790115d5ff9
soheltech <soheltech4@gmail.com>
2026-02-17 04:53:30 +0600

    showing products

--- scripts.js
+++ scripts.js
@@ -5,18 +5,28 @@ const loadProducts = () => {
     .then((products) => TrndingProducts(products));
 };
 
-// {
-//     "id": 11,
-//     "title": "Silicon Power 256GB SSD 3D NAND A55 SLC Cache Performance Boost SATA III 2.5",
-//     "price": 109,
-//     "description": "3D NAND flash are applied to deliver high transfer speeds Remarkable transfer speeds that enable faster bootup and improved overall system performance. The advanced SLC Cache Technology allows performance boost and longer lifespan 7mm slim design suitable for Ultrabooks and Ultra-slim notebooks. Supports TRIM command, Garbage Collection technology, RAID, and ECC (Error Checking & Correction) to provide the optimized performance and enhanced reliability.",
-//     "category": "electronics",
-//     "image": "https://fakestoreapi.com/img/71kWymZ+c+L._AC_SX679_t.png",
-//     "rating": {
-//         "rate": 4.8,
-//         "count": 319
-//     }
-// }
+const loadCategories = () => {
+  const url = "https://fakestoreapi.com/products/categories";
+  fetch(url)
+    .then((res) => res.json())
+    .then((categories) => ShowCaterogries(categories));
+};
+
+
+const ShowCaterogries = (categories) =>{
+  // console.log(categories);
+  const CategoriesItem = document.getElementById("Categories-Item")
+  // console.log(CategoriesItem);
+  CategoriesItem.innerHTML = ""
+  categories.map((catergory) => {
+    const CategoriesDiv = document.createElement("div")
+    CategoriesDiv.innerHTML = `<button class="px-5 py-1 rounded-lg border-1 hover:bg-indigo-600 hover:text-white font-semibold">${catergory}</button>
+`
+    CategoriesItem.appendChild(CategoriesDiv)
+
+  })
+}
+
 
 const TrndingProducts = (products) => {
   const TopProducts = products
@@ -56,4 +66,7 @@ const TrndingProducts = (products) => {
   });
 };
 
+
+
 loadProducts();
+loadCategories()
